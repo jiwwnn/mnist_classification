14ca968adf57fbc9f987949549555bc824087ed2
jiwwnn <134251617+jiwwnn@users.noreply.github.com>
2024-04-15 13:10:47 +0900

    Update model.py

--- template/model.py
+++ template/model.py
@@ -57,7 +57,7 @@ class CustomMLP(nn.Module):
             nn.Linear(36,10),
             nn.Softmax(dim=1)
         )
-    # Total Number of Parameters = 59450 + 1888 + 330 = 61944
+    # Total Number of Parameters = 59450 + 1888 + 330 = 61668
 
     def forward(self, img):
         img = img.reshape(-1, 32*32)
